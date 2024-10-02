c9ce46038c4b571a40750cacd05b6ba773ee44b7
Khưu Hải Châu <haichaukhuu@gmail.com>
2024-10-02 14:03:53 +0700

    rename frontend folder

---
+++ client/src/components/LinksList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+function LinksList({ results }) {
+  return (
+    <Box
+      sx={{
+        marginTop: "72px",
+        // padding: "20px",
+        overflowY: "scroll",
+        height: "calc(100vh - 72px)",
+      }}
+    >
+    </Box>);
+    };
+
+export default LinksList;
---
+++ client/src/components/ShortenerForm.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Box, Typography, TextField, Button } from "@mui/material";
+
+function ShortenerForm (){
+  return (
+    <Box
+      sx={{
+        border: "1px solid #1976D2",
+        borderRadius: "8px",
+        marginTop: "72px",
+        p: 4,
+        display: "inline-block",
+        textAlign: "center",
+      }}
+    >
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{ mb: 2, color: "#1976D2", fontWeight: "bold" }}
+      >
+        Url shortener
+      </Typography>
+
+      <TextField
+        fullWidth
+        variant="outlined"
+        placeholder="Enter long link..."
+        sx={{
+          mb: 2,
+          backgroundColor: "#E0E0E0",
+          borderRadius: "20px",
+          "& .MuiOutlinedInput-root": {
+            borderRadius: "20px",
+          },
+        }}
+      />
+
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "#1976D2",
+          color: "#fff",
+          borderRadius: "20px",
+          padding: "8px 24px",
+          textTransform: "none",
+        }}
+      >
+        create
+      </Button>
+    </Box>
+  );
+};
+
+
+export default ShortenerForm
