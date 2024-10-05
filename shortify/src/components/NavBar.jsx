8eefe8a9feca33d4d0859c490e33bc1cce0c9941
Khưu Hải Châu <haichaukhuu@gmail.com>
2024-10-06 00:22:47 +0700

    refine navbar

--- shortify/src/components/NavBar.jsx
+++ shortify/src/components/NavBar.jsx
@@ -4,30 +4,6 @@ import { useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
   const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userEmail, setUserEmail] = useState('');
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsLoggedIn(true);
-      // Assuming the token contains the user email or you can fetch it from API
-      const userEmailFromToken = localStorage.getItem('userEmail'); // Get email from local storage
-      setUserEmail(userEmailFromToken);
-    }
-  }, []);
-
-  const handleLoginClick = () => {
-    navigate('/login');
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('userEmail'); // Remove user email on logout
-    setIsLoggedIn(false);
-    setUserEmail(''); // Reset user email
-    navigate('/');
-  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -43,20 +19,6 @@ export default function NavBar() {
             Shortify
           </Typography>
 
-          {/* {isLoggedIn ? (
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography variant="body1" sx={{ marginRight: 2 }}>
-                {userEmail}
-              </Typography>
-              <Button onClick={handleLogout} variant="outlined" color="inherit">
-                Logout
-              </Button>
-            </Box>
-          ) : (
-            <Button onClick={handleLoginClick} variant="outlined" color="inherit">
-              Login
-            </Button>
-          )} */}
         </Toolbar>
       </AppBar>
     </Box>
