c324d786b18e97d275df7d6fa5c2cee3920d115d
Khưu Hải Châu <haichaukhuu@gmail.com>
2024-10-06 02:49:43 +0700

    fix api base

--- shortify/src/components/ShortenerForm.jsx
+++ shortify/src/components/ShortenerForm.jsx
@@ -22,7 +22,7 @@ function ShortenerForm({ addLink }) {
 
     try {
       // const response = await axios.post("http://localhost:3000/api/urls/create", {
-      const response = await axios.post(apiBaseUrl +" /api/urls/create", {
+      const response = await axios.post(apiBaseUrl + "/api/urls/create", {
         originalUrl: longUrl,
         customCode: customCode || null
       });
