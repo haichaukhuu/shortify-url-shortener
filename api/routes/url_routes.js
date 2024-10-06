f26b18808217e0ac090ec238521dce76e892dcac
Khưu Hải Châu <haichaukhuu@gmail.com>
2024-10-06 09:41:30 +0700

    fix cannot find module

--- api/routes/url_routes.js
+++ api/routes/url_routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Url = require('../models/url.js');
 
 const { createShortenedUrl, redirectUrl, getAllUrls } = require("../controllers/url_controller");
 
